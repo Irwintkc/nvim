594e6f955f56eed1404aadad328f3d006654d2d0
Irwin Tay <tay@dhcp-67-dist-b-085.epfl.ch>
2026-06-22 14:02:56 +0200

    new config

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -4,13 +4,11 @@ return {
     lazy = false,
     init = function()
       vim.g.vimtex_compiler_method = "latexmk"
-      vim.g.vimtex_compiler_engine = "lualatex"
-      vim.g.vimtex_compiler_latexmk_engines = {
-        _ = "-lualatex",
-      }
+      vim.g.vimtex_compiler_engine = "pdflatex"
 
       vim.g.vimtex_view_method = "sioyek"
       vim.g.vimtex_view_sioyek_exe = "/Applications/sioyek.app/Contents/MacOS/sioyek"
+
       vim.g.vimtex_syntax_conceal_disable = 1
     end,
   },
